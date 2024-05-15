09c8996d0c723b3ea61b296e87b2bfc65f90eafb
Bandaboy <bandatshire@gmail.com>
2024-05-15 16:50:55 +0200

    Stack Example

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,24 @@
-public class Main {
+import java.util.Stack;
 
+public class Main {
+    public static void main(String[] args) {
+        String orWord="Primo";
+        String reversed=reversedWord(orWord);
+        System.out.println("original word: " + orWord);
+        System.out.println("Reversed word: " + reversed);
+
+    }
+
+    public static String reversedWord(String word) {
+        Stack<Character> StackOfChar = new Stack<>();
+        for (int i = 0; i < word.length(); i++) {
+            StackOfChar.push(word.charAt(i));
+        }
+        String reversed = "";
+        while (!StackOfChar.isEmpty()) {
+            reversed += StackOfChar.pop();
+        }
+        return reversed;
+    }
 }
     
